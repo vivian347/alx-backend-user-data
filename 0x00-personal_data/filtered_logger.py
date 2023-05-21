7e9f8154272653c70362694ccc8783993447df6f
vivian347 <vkinyua001@gmail.com>
2023-05-21 06:20:35 +0300

    llogging and password encryption

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -60,7 +60,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields=None):
+    def __init__(self, fields: List[str]):
         """constructor"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields or []

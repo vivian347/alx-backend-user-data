99e9c771b3e6ea4233298f88a699750756cdd574
vivian347 <vkinyua001@gmail.com>
2023-05-21 17:01:00 +0300

    app

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -65,11 +65,10 @@ class Auth:
         if session_id is None:
             return None
         try:
-            found_user = self._db.find_user_by(session_id=session_id)
-            return found_user
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
-
 
     def destroy_session(self, user_id: int) -> None:
         """updates user's session id to None
@@ -77,11 +76,10 @@ class Auth:
         if user_id is None:
             return None
         try:
-            found_user = self._db.find_user_by(id=user_id)
-            self._db.update_user(found_user.id, session_id=None)
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
-
 
     def get_reset_password_token(self, email: str) -> str:
         """generate uuid and update reset_token db field

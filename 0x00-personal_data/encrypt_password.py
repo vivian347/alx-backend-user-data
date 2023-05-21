77ea10c0512bcecfde92e9f30c41dfc00dcdd4bd
vivian347 <vkinyua001@gmail.com>
2023-05-21 06:14:19 +0300

    llogging and password encryption

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,11 +3,13 @@
 """
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     '''returns a salted, hashed password
     '''
     encoded_pwd = password.encode('utf-8')
     return bcrypt.hashpw(encoded_pwd, bcrypt.gensalt())
+
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     ''' validates that the provided pwd matches the hashed pwd

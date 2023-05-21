eb2fa6b86b15bb30422ff01fe0606c6d8855ac29
vivian347 <vkinyua001@gmail.com>
2023-05-21 17:07:04 +0300

    app

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,11 +86,12 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = _generate_uuid()
-            self._db.update_user(user.id, reset_token=reset_token)
-            return reset_token
         except NoResultFound:
             raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+        
 
     def update_password(self, reset_token: str, password: str) -> None:
         """retrieve user using reset_token and re-hash password
